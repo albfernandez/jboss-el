2b1df4217f8eba0b62dace30f524b06f71d1e096
Sander Knopper <sander@knopper-ict.nl>
2021-05-09 00:10:58 +0200

    Remove finalize() and convert to unix line-ending

--- src/test/java/org/jboss/el/util/ReferenceMapTestCase.java
+++ src/test/java/org/jboss/el/util/ReferenceMapTestCase.java
@@ -20,94 +20,95 @@ import org.junit.Test;
  * @author jhook
  */
 public class ReferenceMapTestCase {
-    
+
     public static class Foo {
         private final int id;
         private static int uid = 0;
-        public Foo() { this.id = uid++; }
-        public Foo(int id) { this.id = id; }
+
+        public Foo() {
+            this.id = uid++;
+        }
+
+        public Foo(int id) {
+            this.id = id;
+        }
+
         @Override
-		public String toString() {
+        public String toString() {
             return "Foo[" + this.id + "]";
         }
+
         @Override
-		public boolean equals(Object obj) {
+        public boolean equals(Object obj) {
             return obj instanceof Foo && ((Foo) obj).id == this.id;
         }
-        @Override
-		public int hashCode() { return this.id; }
 
-		@Override
-		protected void finalize() throws Throwable {
-			super.finalize();
-		}
-        
+        @Override
+        public int hashCode() {
+            return this.id;
+        }
     }
-    
+
     public static class Bar {
         private final int id;
         private static int uid = 0;
-        public Bar() { this.id = uid++; }
-        public Bar(int id) { this.id = id; }
-        @Override
-		public String toString() {
-            return "Bar[" + this.id + "]";
+
+        public Bar() {
+            this.id = uid++;
         }
 
-		@Override
-		protected void finalize() throws Throwable {
-			super.finalize();
-		}
+        public Bar(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "Bar[" + this.id + "]";
+        }
     }
-    
+
     public ReferenceMapTestCase() {
-    	super();
+        super();
     }
-    
+
     @Test
     public void testReferences() throws Exception {
-        
-        ReferenceCache<Foo,Bar> map = new ReferenceCache<Foo,Bar>(ReferenceCache.Type.Weak, ReferenceCache.Type.Weak) {
+
+        ReferenceCache<Foo, Bar> map = new ReferenceCache<Foo, Bar>(ReferenceCache.Type.Weak,
+                ReferenceCache.Type.Weak) {
             @Override
-			protected Bar create(Foo key) {
-            	return null;
+            protected Bar create(Foo key) {
+                return null;
             }
         };
-        
-        
+
         Collection<Foo> keys = new ArrayList<Foo>();
         for (int i = 0; i < 1000; i++) {
             keys.add(new Foo(i));
         }
-        
+
         for (Foo f : keys) {
             map.put(f, new Bar(f.id));
         }
-        
-        
+
         for (Foo f : keys) {
             Assert.assertNotNull("Key not null " + f, map.get(f));
         }
-        
+
         // comment and uncomment this line
         keys.clear();
         keys = null;
-        
-        
+
         // Force to GC to reclaim objects
         System.gc();
-        
-        
+
         keys = new ArrayList<Foo>();
         for (int i = 0; i < 1000; i++) {
             keys.add(new Foo(i));
         }
-        
+
         for (Foo f : keys) {
             Assert.assertNull("Key null " + f, map.get(f));
         }
-        
-        
     }
-    
 }
